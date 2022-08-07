2a2fab06c53fd16e30ef47b7748da93ea7692d20
Sanj Akhmedov <1999.sanjar.06@gmail.com>
2022-08-07 22:18:41 +0500

    Task 2 is done

---
+++ Task02/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Enter the first number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the second number: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine($"Max number is: {a}");
+}
+else if (a < b)
+{
+    Console.WriteLine($"Max number is: {b}");
+} else 
+{
+    Console.WriteLine("Numbers are equal");
+}
