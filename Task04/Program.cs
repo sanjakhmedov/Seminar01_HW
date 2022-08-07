6117f849d44d7267c68f8328fc0da3d2685b07da
Sanj Akhmedov <1999.sanjar.06@gmail.com>
2022-08-07 22:24:03 +0500

    tast 4 is done

---
+++ Task04/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Enter the 1st number: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the 2nd number: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the 3rd number: ");
+int n3 = Convert.ToInt32(Console.ReadLine());
+
+if (n1 > n2 && n1 > n3)
+{
+    Console.WriteLine($"Max number is: {n1}");
+}
+else if (n2 > n1 && n2 > n3)
+{
+    Console.WriteLine($"Max number is: {n2}");
+} 
+else if (n3 > n1 && n3 > n2)
+{
+    Console.WriteLine($"Max number is: {n3}");
+}
+else
+{
+    Console.WriteLine("Numbers are equal");
+}
