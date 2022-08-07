cd1089940db69b73e2a4b277a2901fa828cf64fd
Sanj Akhmedov <1999.sanjar.06@gmail.com>
2022-08-08 00:00:46 +0500

    task 07 is done

---
+++ Task07/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Enter 3 digit number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int length = number.ToString().Length;
+int result = 0;
+
+if (length == 3)
+{
+    result = number % 10;
+    Console.WriteLine(result);
+}
+else 
+{
+    Console.WriteLine("Please enter 3 digit number!!!");
+}
