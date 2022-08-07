ae7b9f4aa9d6bcb7b9eef3e689d960af5e7347ed
Sanj Akhmedov <1999.sanjar.06@gmail.com>
2022-08-07 22:59:54 +0500

    task 08 is done

---
+++ Task08/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter the number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int count = 1;
+while (n >= count)
+{
+    if (count % 2 == 0)
+    {
+        Console.Write(count + " ");
+    }
+    count++;
+}
